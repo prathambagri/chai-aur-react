0114699a6e63b9ed2d1158da92d907136a3b92ac
prathambagri <prathambagri2017@gmail.com>
2025-05-23 22:09:18 +0530

    Redux toolkit porject

---
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,29 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos:[{id:1,text:"Hello World"}]
+}
+
+export const todoSlice = createSlice({
+    name:'todo',
+    initialState,
+    reducers:{
+        addTodo: (state, action)=>{
+            const todo={
+                id:nanoid(),
+                text:action.payload,
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state,action)=> {
+            state.todos=state.todos.filter((todo)=>todo.id!==action.payload)
+        },
+        updateTodo: (state, action) =>{
+            const todo = state.todos.find((todo)=>todo.id===action.id)
+            todo.text=action.text
+        }
+    }
+})
+
+export const {addTodo,removeTodo,updateTodo} = todoSlice.actions;
+export default todoSlice.reducer
