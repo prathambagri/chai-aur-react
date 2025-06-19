bb02982096d9c820020156de610ba10e96a3b298
prathambagri <prathambagri2017@gmail.com>
2025-06-19 18:08:52 +0530

    added logo

--- 12MegaBlog/src/components/Logo.jsx
+++ 12MegaBlog/src/components/Logo.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import logo from '../assets/logo.png'
 function Logo({width = '100px'}) {
   return (
-    <div>Logo</div>
+  
+      <img src={logo} alt='Logo' className="h-10 w-auto object-contain"/>
+    
   )
 }
 
